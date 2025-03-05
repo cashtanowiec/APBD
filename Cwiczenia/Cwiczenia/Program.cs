7357d8107df81564ee6404301de6f9ef17f8bf94
s30635 <s30635@pjwstk.edu.pl>
2025-03-05 15:14:09 +0100

    Added getMaximumElement() method

--- Cwiczenia/Cwiczenia/Program.cs
+++ Cwiczenia/Cwiczenia/Program.cs
@@ -9,5 +9,11 @@ static double calculateDoubleAverage(double[] array)
     return array.Average();
 }
 
+static double getMaximumElement(int[] array)
+{
+    return array.Max();
+}
+
 Console.WriteLine(calculateIntAverage([3, 2, 3]));
 Console.WriteLine(calculateDoubleAverage([3.5, 2.2, 3.1]));
+Console.WriteLine(getMaximumElement([3, 2, 15]));

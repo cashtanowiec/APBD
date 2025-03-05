40648954aa35ba217be40c35478b320259ae08b2
s30635 <s30635@pjwstk.edu.pl>
2025-03-05 14:48:36 +0100

    Added calculateAverage()

--- Cwiczenia/Cwiczenia/Program.cs
+++ Cwiczenia/Cwiczenia/Program.cs
@@ -1,1 +1,7 @@
-﻿Console.WriteLine("lalalala");
+﻿using System.Linq;
+static double calculateAverage(int[] array)
+{
+    return array.Average();
+}
+
+Console.WriteLine(calculateAverage([3, 2, 3]));
